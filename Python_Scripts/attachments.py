f4f29503bdb4b6227810920440da696619d6191b
Kai <manicdragon12@gmail.com>
2023-12-26 23:57:58 -0500

    Added baseline support for the "event" attachment

--- Python_Scripts/attachments.py
+++ Python_Scripts/attachments.py
@@ -29,6 +29,8 @@ class attachment():
             self.file(attachments)
         elif self.type == 'split':
             self.split(attachments)
+        elif self.type == 'event':
+            self.event(attachments)
         else:
             print('Unknown attachment type: ' + self.type)
 
@@ -107,3 +109,43 @@ class attachment():
 
     def split(self, attachments):
         self.token = attachments['token']
+        
+    def event(self, attachments):
+        self.event_id = attachments['event_id']
+        self.view = attachments['view']
+        '''
+        Example event:
+        
+        {
+            "attachments": [
+                {
+                    "event_id": "7def5b0a341440e89629ce526ee4ee90",
+                    "view": "brief",
+                    "type": "event"
+                }
+            ],
+            "avatar_url": "https://i.groupme.com/204x204.png.ae6fd52515e747c88db501db9e9fcfd9",
+            "created_at": 1694044794,
+            "favorited_by": [],
+            "group_id": "83901504",
+            "id": "169404479446622258",
+            "name": "GroupMe Calendar",
+            "sender_id": "calendar",
+            "sender_type": "service",
+            "source_guid": "6613d9c02f3f013cb078463b3ba04ca4",
+            "system": false,
+            "text": "'Church Worship Culture' is starting now",
+            "user_id": "calendar",
+            "event": {
+                "type": "calendar.event.starting",
+                "data": {
+                    "call_started": false,
+                    "event_name": "Church Worship Culture",
+                    "minutes": "0"
+                }
+            },
+            "platform": "gm",
+            "pinned_at": null,
+            "pinned_by": ""
+        },
+        '''
